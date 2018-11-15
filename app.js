7fc50ed4d93528c30b8d0bab4a5208dc979d148a
Alex Thompson <alexthompson@Alexs-MacBook.local>
2018-11-15 08:56:19 +0000

    Code Changes

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ app.use(cors());
 const storage = multer.diskStorage({
     destination: "./uploads/",
     filename: function(req, file, cb){
-        cb(null,"FILE-" + Date.now() + path.extname(file.originalname));
+        cb(null,"FILE-" + Date.now() + "-" + file.originalname); 
+        //path.extname(file.originalname) for just file extension 
     }
 });
 
 const upload = multer({
     storage: storage,
-    limits:{fileSize: 1000000},
+    limits:{fileSize: 2000000}, //upload up to 2mb
 });
 
 app.use(function(req, res, next) {
@@ -29,12 +30,10 @@ app.get('/', function(req, res, next) {
 });
 
 app.post('/upload', upload.single('myImage'), function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "POST");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     console.log("Request ---", req.body);
     console.log("Request file ---", req.file);
     res.send("Successful");
+    res.end();
 })
 
 app.listen(8000)
