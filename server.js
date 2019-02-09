9eb85d72ca40dd53f4a27f4de2a03d38c760ee3e
Alex Thompson <alexthompson@Alexs-MacBook.lan>
2019-02-09 20:46:15 +0000

    files for server

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-var path = require("path");
-var multer = require("multer");
-var express = require('express')
-var cors = require('cors');
-var app = express()
+var path = require("path"),
+    multer = require("multer"),
+    fs = require('fs'),
+    http = require('http'),
+    https = require('https'),
+    express = require('express'),
+    cors = require('cors')
 
+var port = 3001
+
+var options = {
+    key: fs.readFileSync('./fileserver.xander.pro.key'),
+    cert: fs.readFileSync('./fileserver.xander.pro.crt'),
+};
+
+var app = express()
 app.use(cors());
+
+var server = https.createServer(options, app).listen(port, function(){
+    console.log("Express server listening on port " + port);
+});
+
+
 
 const storage = multer.diskStorage({
     destination: "./uploads/",
@@ -26,14 +42,23 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function(req, res, next) {
-    
+    res.setTimeout(120000, function(){
+        console.log('Request has timed out.');
+            res.send(408);
+    });
+
+    res.send("No API Here");
+    res.end();
 });
 
 app.post('/upload', upload.single('myImage'), function (req, res, next) {
+    res.setTimeout(120000, function(){
+        console.log('Request has timed out.');
+            res.send(408);
+    });
+
     console.log("Request ---", req.body);
     console.log("Request file ---", req.file);
     res.send("Successful");
     res.end();
 })
-
-app.listen(8000)
